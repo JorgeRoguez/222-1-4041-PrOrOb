44d6931658512adbe2b78fc162078d3599b8f83e
Jorge Roguez <joroguez@gmail.com>
2021-11-03 12:29:56 -0600

    Cambios finales

--- Course.py
+++ Course.py
@@ -2,7 +2,7 @@
 class Course(): 
 
     def __init__(self, id_alumno, nombre_alumno, correo_electronico, calificacion, materia, nombre_maestro):
-        self.id = id_alumno
+        self.id_alumno = id_alumno
         self.nombre_alumno = nombre_alumno
         self.correo_electronico = correo_electronico
         self.calificacion = calificacion
@@ -11,8 +11,19 @@ class Course():
     
     def alumno(self):
         presentacion = ("Id: \t\t{}\nNombre: \t{}\nCorreo: \t{}\nCalificacion: \t{}\nMateria: \t{}\nMaestro: \t{}")
-        print(presentacion.format(self.id, self.nombre_alumno, self.correo_electronico, self.calificacion, self.materia, self.nombre_maestro))
+        print(presentacion.format(self.id_alumno, self.nombre_alumno, self.correo_electronico, self.calificacion, self.materia, self.nombre_maestro))
+
+    def cambio_calif(self):
+        temporal = input("\n¿Desea cambiar la calificacion? (s/n) ")
+        if (temporal == "s"):
+            self.calificacion = input("\n¿Cual es la calificacion nueva? ")
+            Alumno1.alumno()
+            print("Estos son los datos del alumno con la nueva calificacion.")
 
 Alumno1 = Course("0120820333", "Jorge Roguez", "joroguez@gmail.com", 8.5, "PrOrOb", "Luis Guerra")
+Alumno2 = Course("0123456789", "Juan Perez", "juanito@gmail.com", 8.5, "PrOrOb", "Luis Guerra")
 
 Alumno1.alumno()
+Alumno1.cambio_calif()
+print("\n")
+Alumno2.alumno()
